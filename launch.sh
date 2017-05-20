26d2a2d67da06a6937031e729d83c2baf9865eb5
mcmonkey4eva <agoodwin@freneticllc.com>
2017-05-20 15:39:48 -0700

    improve restartability

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 gulp clean min
 dotnet restore
-ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=https://*:8051 dotnet run
+ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=https://*:8051 dotnet run -- $1
